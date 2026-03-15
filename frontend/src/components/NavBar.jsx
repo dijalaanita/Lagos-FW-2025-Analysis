31c0a83e92a839f63168dc54b529c286d2f62e03
Anita Dijala <dijalaanita@gmail.com>
2026-03-15 10:00:54 +0000

    made the nav bar better looking

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,22 +1,42 @@
 import { Link } from "react-router-dom"
 
-export default function NavBar(){
+export default function NavBar({ onLogout }){
 
     return(
         <div style={{
-            padding: "20px",
+            padding: "20px 40px",
             background: "#111",
             color: "white",
             display: "flex",
-            gap: "20px"
+            gap: "20px",
+            justifyContent: "space-between",
+            alignItems: "center",
+            width: "100%",
+            boxSizing: "border-box"
         }}>
-            <Link to="/" style={{color: "white", textDecoration: "none"}}>
-            Overview
-            </Link>
+            <div style={{ display: "flex", gap: "20px"}}>
+                <Link to="/" style={{color: "white", textDecoration: "none", fontWeight: "bold"}}>
+                Overview
+                </Link>
 
-            <Link to="/brand-analysis" style={{color: "white", textDecoration: "none"}}>
-            Brand Analysis
-            </Link>
+                <Link to="/brand-analysis" style={{color: "white", textDecoration: "none", fontWeight: "bold"}}>
+                Brand Analysis
+                </Link>
+            </div>
+
+            <button
+                onClick={onLogout}
+                style={{
+                    background: "transparent",
+                    color: "white",
+                    border: "1px solid white",
+                    padding: "5px 15px",
+                    cursor: "pointer",
+                    borderRadius: "4px",
+                    fontSize: "14px",
+                    fontWeight: "bold"
+                }}
+            >LOGOUT</button>
 
 
 
