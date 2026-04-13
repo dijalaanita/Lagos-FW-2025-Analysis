43aa16ee90cd96d1929d200a58545cdbb7b32056
Anita Dijala <dijalaanita@gmail.com>
2026-04-13 13:34:36 +0000

    push

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
-from .routers import colours, insights, fabrics, users
+from app.routers import colours, insights, fabrics, users
 import os
 import json
 from pathlib import Path
